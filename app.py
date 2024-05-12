111fede8d71938dc8d8d6f5fecb5558ab48d1692
Anh Le <letrananh98@yahoo.com>
2024-05-12 10:50:05 -0400

    added 27.2 practice

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 
 from flask import Flask, render_template, request, redirect
-from models import db, connect_db, Users
+from models import db, connect_db, Users, Post
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 
@@ -20,32 +20,46 @@ connect_db(app)
 
 
 
+@app.route('/')
+def root():
+    """Homepage redirects to list of users."""
+
+    return redirect("/users")
+
+
 @app.route('/users')
 def home_page():
     """SHOW LIST OF ALL USERS"""
     user = Users.query.all()
-    return render_template('list.html', user = user)
+    return render_template('users.html', user = user)
+
+@app.route('/users/new', methods=["GET"])
+def user_new_form():
+    """Show a form to create a new user"""
+    return render_template('users/new.html')
 
 @app.route('/users/new', methods=['POST'])
 def create_user():
-    first_name = request.form("first name")
-    last_name = request.form("last name")
-    url = request.form("url")
-
-    new_user = Users(first_name = first_name, last_name = last_name, url = url)
+    
+    new_user = Users(
+	        first_name=request.form['first_name'],
+	        last_name=request.form['last_name'],
+	        image_url=request.form['image_url'] or None)
+    
     db.session.add(new_user)
     db.session.commit()
-    return redirect(f"/new_user.id")
+    return redirect(f"users")
 
 
-@app.route("/users/<int:users.id>")
-def show_user():
-    """SHOW DETAIL ABOUT A SINGLE USER"""
-    user = Users.query.get_or_404(Users.id)
-    return render_template('user_detail_page.html', user = user)
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+ """Show a page with info on a specific user"""
+ user = Users.query.get_or_404(user_id)
+ return render_template('users/show.html', user=user)
 
-@app.route("/users/<int:users.id>/edit")
+@app.route("/users/<int:users.id>/edit", methods = ["POSTS"])
 def update_user():
+
     first_name = request.form(first_name)
     last_name = request.form(last_name)
     url = request.form(url)
@@ -56,15 +70,65 @@ def update_user():
 
     return redirect(f"/users/<int:users.id>")
 
-@app.route("/users/<int:users.id>/delete")
-def remove_user():
+@app.route("/users/<int:users.id>/delete", methods=["POST"])
+def remove_user(user_id):
     """Removing a user"""
-    first_name = request.form(first_name)
-    last_name = request.form(last_name)
-    url = request.form(url)
-
-    delete_user = Users(first_name = first_name, last_name = last_name, url = url)
-    db.session.delete(delete_user)
+    user = Users.query.get_or_404(user_id)
+    db.session.delete(user)
     db.session.commit()
 
     return redirect("/users")
+
+
+####################################################################################################
+##PART 2##  
+
+@app.route("/users/<int:users.id>/posts/new", methods=['GET'])
+def show_post_form():
+    return render_template("post_form.html")
+
+
+app.route("/users/<int:users.id>/post/new", methods= ['POST'])
+def handle_add_form():
+
+    title = request.form("title")
+    content = request.form("content")
+
+    new_post = Post(title = title, content = content)
+    db.add(new_post)
+    db.commit()
+
+    return redirect("user.html")
+
+
+@app.route("/post/<int:post.id>")
+def the_post():
+
+    return render_template("post.html")
+
+@app.route("posts/<int:post.id>/edit", methods=["GET"])
+def show_edit_form():
+
+    return render_template("post_edit.html")
+
+@app.route("posts/<int:post.id>/edit", methods=["POST"])
+def handle_edit_form():
+    title = request.form("title")
+    content = request.form("content")
+
+    post = Post(title = title, content = content)
+    db.add(post)
+    db.commit()
+
+    return redirect(f"/users/{post.user_id}")
+
+@app.route("/posts/<int:post.id>/delete", methods=["POST"])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect (f"/users/{post.user_id}")
+
+####################################################################################################
--- models.py
+++ models.py
@@ -7,13 +7,19 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
 
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
 class Users(db.Model):
 
     __tablename__ = "users"
 
-    def __repr__(self):
-        p = self
-        return f"<User id{p.id} first name{p.first_name} last_name{p.last_name}"
+    
+
+    # def full_name(self):
+    #     """Return full name of user."""
+    #     return f"{self.first_name} {self.last_name}"
+	
 
     id = db.Column(db.Integer,
                    primary_key=True,
@@ -29,4 +35,46 @@ class Users(db.Model):
     
     image_url = db.Column(db.Text,
                      nullable=True,
-                     unique=True)
+                     unique=True, default = DEFAULT_IMAGE_URL)
+    
+    def full_name(self):
+        return f"{self.first_name}{self.last_name}"
+# def __repr__(self):
+    #     p = self
+    #     return f"<User id{p.id} first name{p.first_name} last_name{p.last_name}"
+
+#####################################################################################################
+##PART 2##
+
+class Post(db.Model):
+    __tablename__ = 'Posts'
+    
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    
+    title = db.Column(db.Text, nullable = False)
+
+    content = db.Column(db.Text, nullable = False)
+
+    created_at = db.Column(db.DateTime,
+        nullable=False)
+    
+    user_id = db.Column(db.Text, db.ForeignKey("'users.id'"), nullable = False)
+
+def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from app import app
+    connect_db(app)
+
+    db.drop_all()
+    db.create_all()
